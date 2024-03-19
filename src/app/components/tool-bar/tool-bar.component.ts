2e7d6e7a5e10d7b8e5ad9935593daf1838ed87c5
nataliat21 <natalia.torozco21@gmail.com>
2024-03-18 20:35:22 -0500

    Functionality is added to the menu on mobile devices, and styles have also been added

--- src/app/components/tool-bar/tool-bar.component.ts
+++ src/app/components/tool-bar/tool-bar.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './tool-bar.component.scss'
 })
 export class ToolBarComponent {
+  openMenu: boolean = false;
+  showInformation: boolean = false;
+
+  toggleMenu() {
+    this.openMenu = !this.openMenu;
+    if (this.openMenu) {
+      this.showInformation = false; // Si se abre el menú, asegúrate de que la información esté cerrada
+    }
+  }
+
+  toggleInformation() {
+    this.showInformation = !this.showInformation;
+    if (this.showInformation) {
+      this.openMenu = false; // Si se muestra la información, asegúrate de que el menú esté cerrado
+    }
+    else if (!this.showInformation) {
+      this.openMenu = true; // Si se cierra la información, asegúrate de que el menú esté abierto
+    }
+  }
 
 }
